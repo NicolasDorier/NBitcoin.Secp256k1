72e58616b23b07d66a0f8883023fa1c83d0e17c9
nicolas.dorier <nicolas.dorier@gmail.com>
2019-04-25 14:34:07 +0900

    Fix build script for powershell

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ docker build -t secp256k1 .
 If(test-path "bin")
 {
     Remove-Item -Recurse -Force "bin"
+    Start-Sleep -Seconds 1
 }
 New-Item -ItemType Directory -path "bin"
 docker run -v "$(Get-Location)/bin:/data" secp256k1
